cde401c7f7fcc22485cab38efee70692544c33be
Serhan Konukluk <serhankonukluk@gmail.com>
2021-01-25 13:14:05 +0300

    calismalar

---
+++ calismalar/ucgen_alan_hesaplama.py
@@ -0,0 +1,5 @@
+taban = float(input('Üçgenin taban uzunluğunu giriniz: '))
+yukseklik = float(input('Üçgenin yüksekliğini giriniz: '))
+
+alan = (taban * yukseklik) / 2
+print(f'Üçgenın alanı: {alan} cm2')
