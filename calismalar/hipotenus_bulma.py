b0a440fdd7d490c9881a7e16bba4b9cdeec21f33
Serhan Konukluk <serhankonukluk@gmail.com>
2021-01-25 14:49:14 +0300

    calismalar

---
+++ calismalar/hipotenus_bulma.py
@@ -0,0 +1,6 @@
+a_kenari = int(input('Üçgenin "a" kenarının uzunluğunu giriniz: '))
+b_kenari = int(input('Üçgenin "b" kenarının uzunluğunu giriniz: '))
+
+hipotenus = ((a_kenari ** 2) + (b_kenari ** 2)) ** 0.5
+
+print(f'Hipotenüs: {int(hipotenus)}')
