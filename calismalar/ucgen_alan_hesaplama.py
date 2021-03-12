48a3f5521944b137cecb561ed623e3105da68cec
Serhan Konukluk <serhankonukluk@gmail.com>
2021-03-12 16:40:58 +0300

    duzenleme

--- calismalar/ucgen_alan_hesaplama.py
+++ calismalar/ucgen_alan_hesaplama.py
@@ -1,5 +1,4 @@
-taban = float(input('Üçgenin taban uzunluğunu giriniz: '))
-yukseklik = float(input('Üçgenin yüksekliğini giriniz: '))
 
-alan = (taban * yukseklik) / 2
-print(f'Üçgenın alanı: {alan} cm2')
+def calculate_triangle_area(base, height):
+    area = (base * height) / 2
+    return f'Area of the triangle: {area}'
