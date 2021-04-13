1f59ed0acca812a449d2cbdff9c83387e41de5c8
Serhan Konukluk <serhankonukluk@gmail.com>
2021-04-13 16:27:21 +0300

    calculates the greatest common divisor of two numbers

--- calismalar/greatest_common_divisor.py
+++ calismalar/greatest_common_divisor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def greatest_common_divisor(first_number, second_number):
     if first_number >= second_number:
         the_biggest_number = first_number
@@ -10,3 +12,27 @@ def greatest_common_divisor(first_number, second_number):
             common_divisors.append(number)
     max_common_divisor = max(common_divisors)
     return max_common_divisor
+fillchar_len = 100
+
+print(f'''
+{'=' * fillchar_len}
+{'GREATEST COMMON DIVISOR CALCULATOR'.center(fillchar_len)}
+{'=' * fillchar_len}
+This program calculates the GCD of a given two number.
+
+->Please enter only numbers.
+->Type 0 (Zero) to exit.
+{'-' * fillchar_len}''')
+while True:
+    try:
+        print('Enter the first number:')
+        first_int = int(input('>>>'))
+        if first_int == 0:
+            exit('Exiting...')
+        print('Enter the second number:')
+        second_int = int(input('>>>'))
+        if second_int == 0:
+            exit('Exiting...')
+        print(greatest_common_divisor(first_int, second_int))
+    except ValueError:
+        print('Please enter a valid number!')
