95bcfb2a5f9ff695f4f787d7ef2c48b040083618
Serhan Konukluk <serhankonukluk@gmail.com>
2021-04-13 11:12:48 +0300

    calistirmaya hazir hale getirildi

--- calismalar/aritmetik_ortalama_ve_standart_sapma.py
+++ calismalar/aritmetik_ortalama_ve_standart_sapma.py
@@ -11,4 +11,43 @@ def get_stdev(iterable):
     for i in iterable:
         total += (i - average) ** 2
     stdev = sqrt(total / (count - 1))
-    return stdev, average
+    return f'Standard Deviation: {stdev} | Average: {average}'
+
+def intro(title):
+    fillchar_len = 75
+    print('=' * fillchar_len)
+    print(str(title).center(fillchar_len).upper())
+    print('=' * fillchar_len)
+    description = '''This program calculates the standard deviation and average of given numbers.
+You should only enter numbers. 
+    
+->Enter 1 number at a time.
+->Press "q" to end the entering number.'''
+    print(description)
+    print('=' * fillchar_len)
+    print('Enter the numbers'.center(fillchar_len))
+    print('-' * fillchar_len)
+
+
+numbers_list = []
+result = 0
+
+intro('Standard Deviation Calculator')
+while True:
+    numbers = input('>>>')
+    if numbers == 'q':
+        break
+    else:
+        try:
+            if numbers.__contains__('.'):
+                numbers_list.append(float(numbers))
+            else:
+                numbers_list.append(int(numbers))
+        except:
+            print('Please enter a valid number.')
+            continue
+
+
+standard_deviation = get_stdev(numbers_list)
+print(f'Entered numbers: {numbers_list}')
+print(standard_deviation)
